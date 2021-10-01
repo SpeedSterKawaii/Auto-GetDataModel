dca571cb5300fb69e27f4cb147d80e6d31947b08
SpeedSterKawaii <72403623+SpeedSterKawaii@users.noreply.github.com>
2021-10-01 09:54:37 -0400

    Update Auto-GDM.cpp

--- Auto-GDM.cpp
+++ Auto-GDM.cpp
@@ -1,3 +1,26 @@
 /*
-
+  Grabs the GDM from the Internet :)
+  Credit me if your going to use it.
 */
+
+// Hi roblox why are you reading this?
+
+#define Bypass(e) (e - 0x400000 + (DWORD)GetModuleHandleA(0))
+
+int GDM_Offset = 0; // let this be 0 dont update it.
+#define GDM_Convention __thiscall // prob nevar updates
+
+typedef DWORD(GDM_Convention* my_gdm)(DWORD g, DWORD a);
+my_gdm GDM_Usage = (my_gdmc)(Bypass(GDM_Offset));
+
+bool DidItReplace()
+{
+  return GDM_Offset == NULL;
+}
+
+static int MY_MAIN()
+{
+   MessageBoxA(NULL, "Grabbing latest get-data-model.", "Auto GDM", NULL);
+   GDM_Offset = 0;//hahahhahfahfafh U THOUGHT U SKID
+   std::cout << "Ok, GDM is "<< GDM_Offset << endl; //std 
+}
