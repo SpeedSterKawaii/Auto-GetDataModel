9ae4b81ea0d24f1518e95b1923a3c9c771071e61
SpeedSterKawaii <72403623+SpeedSterKawaii@users.noreply.github.com>
2021-10-01 10:03:15 -0400

    Update Auto-GDM.cpp

--- Auto-GDM.cpp
+++ Auto-GDM.cpp
@@ -5,7 +5,53 @@
 
 // Hi roblox why are you reading this?
 
+#include <Windows.h>
+#include <string>
+#include <WinInet.h>
+#pragma comment(lib,"ws2_32.lib")
+#pragma comment(lib, "wininet.lib")
+
+std::string replaceAll(std::string subject, const std::string& search,//cringy
+	const std::string& replace) {
+	size_t pos = 0;
+	while ((pos = subject.find(search, pos)) != std::string::npos) {
+		subject.replace(pos, search.length(), replace);
+		pos += replace.length();
+	}
+	return subject;
+}
+
+std::string GrabStringFromURL(const char* URL) //cringy
+{
+	HINTERNET interwebs = InternetOpenA("Mozilla/5.0", INTERNET_OPEN_TYPE_DIRECT, NULL, NULL, NULL);
+	HINTERNET urlFile;
+	std::string rtn;
+	if (interwebs) {
+		urlFile = InternetOpenUrlA(interwebs, URL, NULL, NULL, NULL, NULL);
+		if (urlFile) {
+			char buffer[2000];
+			DWORD bytesRead;
+			do {
+				InternetReadFile(urlFile, buffer, 2000, &bytesRead);
+				rtn.append(buffer, bytesRead);
+				memset(buffer, 0, 2000);
+			} while (bytesRead);
+			InternetCloseHandle(interwebs);
+			InternetCloseHandle(urlFile);
+			std::string p = replaceAll(rtn, "|n", "\r\n");
+			return p;
+		}
+	}
+	InternetCloseHandle(interwebs);
+	std::string p = replaceAll(rtn, "|n", "\r\n");
+	return p;
+}
+
 #define Bypass(e) (e - 0x400000 + (DWORD)GetModuleHandleA(0))
+
+string GDM = GrabStringFromURL("https://raw.githubusercontent.com/SpeedSterKawaii/Auto-GetDataModel/main/GDM.txt");//DO NOT CHANGE!
+ 
+
 
 int GDM_Offset = 0; // let this be 0 dont update it.
 #define GDM_Convention __thiscall // prob nevar updates
@@ -13,7 +59,7 @@ int GDM_Offset = 0; // let this be 0 dont update it.
 typedef DWORD(GDM_Convention* my_gdm)(DWORD g, DWORD a);
 my_gdm GDM_Usage = (my_gdmc)(Bypass(GDM_Offset));
 
-bool DidItReplace()
+bool DidItReplace()//did it replace? nope!
 {
   return GDM_Offset == NULL;
 }
@@ -21,6 +67,8 @@ bool DidItReplace()
 static int MY_MAIN()
 {
    MessageBoxA(NULL, "Grabbing latest get-data-model.", "Auto GDM", NULL);
-   GDM_Offset = 0;//hahahhahfahfafh U THOUGHT U SKID
-   std::cout << "Ok, GDM is "<< GDM_Offset << endl; //std 
+   int GDM_Grabbed = std::stoi(GDM);
+   GDM_Offset = GDM_Grabbed;//switch it up
+   MessageBoxA(NULL, GDM_Offset, "Hehe-GDM", NULL);
+   getchar(); exit(NULL);
 }
